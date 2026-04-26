e26baaacd9a5a8207e31a68ecd95ff3cb50b25f5
123artCode <150064063+123artCode@users.noreply.github.com>
2026-04-26 14:36:12 +0200

    Initial check-in

---
+++ Assets/main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // double mass = density * spere.transform.localScale;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
